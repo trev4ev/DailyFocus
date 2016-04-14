7620dad61396a658d6891e9160cbd30f4aa4d468
Trevor <trevoraquino@gmail.com>
2016-04-14 11:59:08 -0700

    add reset button

--- app.js
+++ app.js
@@ -18,10 +18,34 @@ $('#textInput').keypress( function(e) {
 });
 
 function goalAdded() {
-    document.title = localStorage.getItem("goal");
     var h1 = $("#title");
     h1.text(localStorage.getItem("goal"));
     h1.css("fontSize","18vh");
-    h1.css("marginTop","35vh");
-    $("#textInput").css("visibility","hidden");
+    h1.css("marginBottom","0");
+    
+    var textInput = $("#textInput");
+    textInput.css("visibility","hidden");
+    textInput.css("height","0");
+    
+    var h2 = $("#header");
+    h2.css("visibility","visible");
+    
+    $("#resetButton").css("visibility","visible");
+}
+
+function reset() {
+    localStorage.setItem("goal",null);
+    localStorage.setItem("day",null);
+    
+    var h1 = $("#title");
+    h1.text("What is your focus for today?");
+    h1.css("fontSize","10vh");
+    h1.css("marginBottom","4.5vh");
+    
+    $("#textInput").css("visibility","visible");
+    $("#textInput").css("height","5vh");
+    
+    var h2 = $("#header");
+    h2.css("visibility","hidden");
+    $("#resetButton").css("visibility","hidden");
 }
